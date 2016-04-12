8376ccafc066e0b478d8413cf5ba28e96d20df88
George Vauter <george.vauter@inin.com>
2016-04-12 15:31:03 -0400

    fixing hard coded port and usig arg

--- openstack_exporter/exporter.py
+++ openstack_exporter/exporter.py
@@ -6,9 +6,9 @@ from prometheus_client.core import REGISTRY
 from prometheus_client import generate_latest, start_http_server
 
 
-def start_exporter(config):
+def start_exporter(config, port):
     REGISTRY.register(NovaCollector(config))
-    start_http_server(8000)
+    start_http_server(port)
     while True:
         generate_latest(REGISTRY)
         time.sleep(30)
