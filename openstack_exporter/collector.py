fc2689db1338a588872a59d087b02b0e868aa9c1
George Vauter <george.vauter@inin.com>
2016-04-19 15:34:58 -0400

    adding in nova limit metrics

--- openstack_exporter/collector.py
+++ openstack_exporter/collector.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
+""" OpenStack metrics exporter for prometheus.io"""
 
 from novaclient import client as nova_client
+from keystoneclient.v2_0 import client as keystone_client
 from prometheus_client.core import GaugeMetricFamily
 
 
@@ -15,28 +17,56 @@ class NovaCollector(object):
         password = self.config['openstack']['password']
         tenant = self.config['openstack']['tenant']
         auth_url = self.config['openstack']['auth_url']
+
         nova = nova_client.Client("2", username, password, tenant, auth_url)
 
-        METRIC_PREFIX = 'openstack_nova'
-        LABELS = ['hypervisor']
+        keystone = keystone_client.Client(
+                   username = username,
+                   password = password,
+                   tenant_name = tenant,
+                   auth_url = auth_url)
 
-        metrics = ['current_workload', 'running_vms', 'vcpus', 'vcpus_used', 'memory_mb', 'memory_mb_used',
-                   'free_ram_mb', 'local_gb', 'local_gb_used', 'free_disk_gb']
+
+        METRIC_PREFIX = 'openstack_nova'
+
+        TENANT_METRICS = ['maxTotalInstances', 'totalInstancesUsed',
+                          'maxTotalRAMSize', 'totalRAMUsed',
+                          'maxTotalCores', 'totalCoresUsed',
+                          'maxTotalFloatingIps', 'totalFloatingIpsUsed',
+                          'maxSecurityGroups', 'totalSecurityGroupsUsed',
+                          'maxServerGroups', 'totalServerGroupsUsed']
+
+        HYPERVISOR_METRICS = ['current_workload', 'running_vms', 'vcpus', 'vcpus_used',
+                              'memory_mb', 'memory_mb_used', 'free_ram_mb', 'local_gb',
+                              'local_gb_used', 'free_disk_gb']
         gauges = {}
 
-        for metric in metrics:
+        for metric in HYPERVISOR_METRICS:
             gauges[metric] = GaugeMetricFamily('%s_%s' % (METRIC_PREFIX, metric),
                                                '%s' % metric,
                                                value=None,
-                                               labels=LABELS)
+                                               labels=['hypervisor'])
+
+        for metric in TENANT_METRICS:
+            gauges[metric] = GaugeMetricFamily('%s_%s' % (METRIC_PREFIX, metric),
+                                               '%s' % metric,
+                                               value=None,
+                                               labels=['tenant'])
 
         hypervisors = nova.hypervisors.list()
+        tenants = keystone.tenants.list()
 
-        for h in hypervisors:
-            for metric in metrics:
-                gauges[metric].add_metric([h.hypervisor_hostname], getattr(h, metric))
+        for tenant in tenants:
+            limits = nova.limits.get(tenant.name)._info['absolute']
+            for metric in TENANT_METRICS:
+                gauges[metric].add_metric([tenant.name], limits[metric])
 
-        for metric in metrics:
+        for hypervisor in hypervisors:
+            for metric in HYPERVISOR_METRICS:
+                gauges[metric].add_metric([hypervisor.hypervisor_hostname],
+                                          getattr(hypervisor, metric))
+
+
+        for metric in (TENANT_METRICS + HYPERVISOR_METRICS):
             yield gauges[metric]
-
 
